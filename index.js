1fa0e014c46f8f8c3e3b5b522fa6721fa0f83254
Morklympious <34610940+Morklympious@users.noreply.github.com>
2019-11-10 18:09:52 -0800

    rework the structure of stuff, pep up index so I can publish to npm sometime

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const glob = require("glob");
 const fs = require("fs");
+const util = require("util");
 
 const hands = glob.sync("./attacks/barehands/*.js");
 const blade = glob.sync("./attacks/sword/*.js");
 
-const barehands = [ hands[0] ].map((file) => {
-    const attack = fs.readFileSync(file, "utf-8");
-    const search = new RegExp("/")
-    console.log(typeof attack, attack,);
-
-    const a = attack.replace(/"},"/g, "AAAA")
-    console.log(a);
-});
-
+const barehands = hands.map((file) => require(file));
 const sword = blade.map((file) => require(file));
 
+if(process.env.GENERATE) {
+    fs.writeFileSync("./dist/barehands.json", JSON.stringify(barehands, null, 4));
+    fs.writeFileSync("./dist/barehands.js", `module.exports = ${util.inspect(barehands)}`);
+}
 
 module.exports = {
     barehands,
     sword,
 };
-
-
