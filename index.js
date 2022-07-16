848695d829ebf372f1b227639421c881c1275d6d
Morklympious <Morklympious@gmail.com>
2022-07-15 17:39:54 -0700

    [BUILD] Rename output folder, update package-lock

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const glob = require("glob");
 const fs = require("fs");
-const util = require("util");
+const path = require("path");
 
-const hands = glob.sync("./attacks/barehands/*.js");
-const blade = glob.sync("./attacks/sword/*.js");
+const PUBLIC_DIR = "./public";
+
+const hands = glob.sync("./src/vanilla/barehands/*.js");
+const blade = glob.sync("./src/vanilla/sword/*.js");
 
 const barehands = hands.map((file) => require(file));
 const sword = blade.map((file) => require(file));
 
 if(process.env.GENERATE) {
-    fs.writeFileSync("./dist/barehands.json", JSON.stringify(barehands, null, 4));
-    fs.writeFileSync("./dist/sword.json", JSON.stringify(sword, null, 4))
+    fs.writeFileSync(path.join(PUBLIC_DIR, "barehands.json"), JSON.stringify(barehands, null, 4));
+    fs.writeFileSync(path.join(PUBLIC_DIR, "sword.json"), JSON.stringify(sword, null, 4))
 }
 
 module.exports = {
