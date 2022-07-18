e5dfdc38ed5cae3fa59f8654d521ce270c1e45d2
Morklympious <Morklympious@gmail.com>
2022-07-17 22:44:58 -0700

    [BUILD] Generate individual barehands files

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ const PUBLIC_DIR = "./public";
 const hands = glob.sync("./src/vanilla/barehands/*.js");
 const blade = glob.sync("./src/vanilla/sword/*.js");
 
-const barehands = hands.map((file) => require(file));
+fs.rmSync(PUBLIC_DIR, { recursive: true, force: true })
+fs.mkdirSync(PUBLIC_DIR);
+
+// Outputs the move in a single-file version in addition to barehands / sword
+const singlify = (file) => fs.writeFileSync(path.join(PUBLIC_DIR, `${path.parse(file).name}.json`), JSON.stringify(data, null, 4));
+
+const barehands = hands.map((file) => {
+    const data = require(file);
+    return data;
+});
+
 const sword = blade.map((file) => require(file));
+
+console.log(barehands)
 
 if(process.env.GENERATE) {
     fs.writeFileSync(path.join(PUBLIC_DIR, "barehands.json"), JSON.stringify(barehands, null, 4));
